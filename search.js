86240d37338d7ad3239f7344f878bb771e16feb2
Anhar01 <152172958+Anhar01@users.noreply.github.com>
2023-11-27 19:38:06 +0800

    Add files via upload

---
+++ search.js
@@ -0,0 +1,14 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () =>{
+    search.classList.toggle('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+}
